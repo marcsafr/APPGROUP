37987e61ecf9cc3c0d006ecc02ac9eb2180b9a8b
Pau Aumedes <paumedes@gmail.com>
2019-05-03 19:16:40 +0200

    Login provisional

---
+++ Login/conn.php
@@ -0,0 +1,7 @@
+<?php
+// Connection variables
+$dbhost	= "localhost";	   // localhost or IP
+$dbuser	= "root";		  // database username
+$dbpass	= "root";		     // database password
+$dbname	= "phplogin";    // database name
+?>
